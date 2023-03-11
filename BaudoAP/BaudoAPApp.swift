4c44939c15664d0224344150a7d22abfca75c612
Steven Hernandez <stevendezc@gmail.com>
2023-03-11 10:42:06 -0500

    Initial Commit

---
+++ BaudoAP/BaudoAPApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaudoAPApp.swift
+//  BaudoAP
+//
+//  Created by Codez on 11/03/23.
+//
+
+import SwiftUI
+
+@main
+struct BaudoAPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
